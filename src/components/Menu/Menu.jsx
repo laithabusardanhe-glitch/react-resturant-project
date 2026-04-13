e19dd90636d19222388678ade34869028aa2c364
laithabusardanhe-glitch <laithabusardanhe@gmail.com>
2026-04-13 22:05:49 +0300

    set up react project with vite

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,27 @@
+import {Container, Typography } from "@mui/material"
+import App from "../../App"
+function Menu() {
+    return(
+
+        <>
+        <Container>
+      <Typography>Menu</Typography>
+
+
+
+        </Container>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </>
+    )
+
+
+}
+export default Menu ;
